67aea3cc81f1ecdc89e74a08b765749d95eb5e5b
Jassim Hayat <jassim.hayat@gmail.com>
2025-01-28 20:35:00 +0300

    Controlled Forms in React Lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
 // src/App.jsx
+import { use, useState } from 'react';
+import './App.css';
+import Bookshelf from './components/Bookshelf/Bookshelf.jsx';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+
+  let initialState = {
+
+        title : '',
+        author : '',
+      }
+
+     const [books, setBooks] = useState([
+    { title: 'Fourth Wing', author: 'Rebecca Yarros' },
+    { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
+
+  ]);
+  
+
+  const [newBook,setNewBooks] = useState ({initialState})
+ 
+  const handleInputChange = (event) => {
+    const newBookData = {...newBook,[event.targt.name]:event.targt.value}
+    
+  }
+  // const handleInputChange = (event)=> {
+
+  return (
+    <>
+      <h1>My Bookshelf</h1>
+      <Bookshelf />
+    </>
+  );
 };
 
 export default App;
